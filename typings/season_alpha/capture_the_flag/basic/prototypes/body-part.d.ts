38caaabe6e55e7e6d748a38f85e763635229d0a9
ewoifuoi <hamimelons@126.com>
2023-06-15 20:50:48 +0800

    初始化

---
+++ typings/season_alpha/capture_the_flag/basic/prototypes/body-part.d.ts
@@ -0,0 +1,13 @@
+declare module "arena/season_alpha/capture_the_flag/basic/prototypes" {
+    import {BodyPartType, GameObject} from "game/prototypes";
+
+    /** A separate part of creep body */
+    export class BodyPart extends GameObject {
+
+        /** The type of the body part */
+        readonly type: BodyPartType;
+
+        /** The number of ticks until this object disappears */
+        readonly ticksToDecay: number;
+    }
+}
